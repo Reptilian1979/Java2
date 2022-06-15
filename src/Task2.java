61f348c335c4bb63463046d0c56ec90f9e4af6d3
Reptilian1979 <alexter1979@gmail.com>
2022-06-15 04:08:21 +0300

    Homework done

--- src/Task2.java
+++ src/Task2.java
@@ -1,2 +1,12 @@
+import java.security.Principal;
+
 public class Task2 {
+    public static void main(String[] args) {
+        int accountAmount = 50;
+        int payment = 1021;
+        boolean necessaryBonus = payment >= 1000;
+        int bonus = necessaryBonus ? (payment / 100) : 0;
+        System.out.println("Сумма на счете: " + (accountAmount + payment + bonus));
+        System.out.println("Из них начисленных бонусов: " + bonus);
+    }
 }
